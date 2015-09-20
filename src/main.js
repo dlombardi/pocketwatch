735d4052026e321268ae94bdbd5acec218cb2d37
berkeleybear <alicewu1992@gmail.com>
2015-09-19 17:31:43 -0700

    trying to push to firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var scotchApp = angular.module('scotchApp', ['ui.router']);
 var ref = new Firebase("https://tc-pocketwatch.firebaseio.com/");
 var Locations = ref.child('tracked-locations');
+var Users = ref.child('users');
 
 scotchApp.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('#');
@@ -19,14 +20,13 @@ scotchApp.config(function($stateProvider, $urlRouterProvider){
 
 scotchApp.controller('mainController', function($scope){
   $scope.message = "";
-
+  $scope.login = function() {
+    userData = {userData: [$scope.userName, $scope.userEmail]}
+    Users.push(userData);
+  }
 });
 
 scotchApp.controller('addLocationsController', function($scope, $http){
 
-
-
   $scope.message = "enter your location or the location of a person you care about to receive notifications when thy experience severe weather";
-
-
 });
